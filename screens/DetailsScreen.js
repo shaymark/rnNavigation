51248ecb38aa6ae9fbdbb4c04e460c0da287c3a6
shay markovich <shaymark@gmail.com>
2019-09-07 23:14:47 +0300

    add details

--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -1,28 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Button, View, Text,StyleSheet } from 'react-native';
 import { ExpoConfigView } from '@expo/samples';
 
-export default function DetailsScreen() {
-   return (
-     <View style={styles.container}>
-         <Text style={styles.text}>hello world</Text>
-         <Text style={styles.text}>hello world</Text>
-     </View>
-   )
-}
-
-DetailsScreen.navigationOptions = {
-  title: 'Detailss',
-};
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    backgroundColor: '#0ff'
-  },
-  text: {
-    padding: 10,
+export default class DetailsScreen extends Component {
+  componentDidMount() {
+    const { navigation } = this.props.navigation;
+    alert(JSON.stringify(this.props.navigation.getParam("a", "defaultValue")));
   }
-});
+
+  render() {
+     return (
+       <View style={this.styles.container}>
+           <Text style={this.styles.text}>hello world</Text>
+           <Text style={this.styles.text}>hello world</Text>
+       </View>
+     )
+  }
+
+  navigationOptions = {
+    title: 'Detailsssssaaa',
+  }
+
+
+   styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 20,
+      backgroundColor: '#0ff'
+    },
+    text: {
+      padding: 10,
+    }
+  })
+}
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 import { Button, View, Text } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import { withNavigation } from 'react-navigation';
+import DialogInput from 'react-native-dialog-input';
+import {AsyncStorage} from 'react-native';
 
 class Homescreen extends Component {
 
-    state = { isShowingText: true };
+    state = {
+      isShowingText: false ,
+      isDialogVisible: false,
+    };
 
     componentDidMount() {
       const { navigation } = this.props;
@@ -39,21 +44,121 @@ class Homescreen extends Component {
           title="Go to Details s"
           onPress={() => this.props.navigation.navigate('Details')}
           />
+          <Button
+          title="Go to Details s with passwords"
+          onPress={() => this.navigateToDetails()}
+          />
 
           <Button
           title="hide show title"
           onPress={() => this.setState({ isShowingText: !this.state.isShowingText })}
           />
           <Button
+          title="show dialog"
+          onPress={() => this.setState({ isDialogVisible: true })}
+          />
+          <Button
           title="show navigation object"
           onPress={() => alert(JSON.stringify(this.props.navigation))}
           />
+          <Button
+          title="store objectsss"
+          onPress={() => this.storeObject({a: "a"})}
+          />
+          <Button
+          title="restore objectsss"
+          onPress={() => this.retrieveObject()}
+          />
+          <Button
+          title="store data"
+          onPress={() => this._storeData()}
+          />
+          <Button
+          title="get data"
+          onPress={() => this._retrieveData()}
+          />
+          <DialogInput isDialogVisible={this.state.isDialogVisible}
+            title={"DialogInput 1"}
+            message={"Message for DialogInput #1"}
+            hintInput ={"HINT INPUT"}
+            submitInput={ (inputText) => {this.sendInput(inputText)} }
+            closeDialog={ () => {this.showDialog(false)}}>
+          </DialogInput>
       </View>
-
-
-
     )
   }
+
+  showDialog(isVisable){
+    this.setState({
+      isDialogVisible: isVisable,
+    })
+  }
+
+  sendInput(inputText){
+    this.checkPassword(inputText)
+  }
+
+  checkPassword(password){
+    if(password == '1234') {
+      this.showDialog(false)
+    } else {
+      alert('wrong password')
+    }
+  }
+
+  async navigateToDetails(){
+    const passwords = await this.retrieveObject()
+    this.props.navigation.navigate('Details', passwords)
+  }
+
+  async storeObject(ob) {
+      try {
+        await AsyncStorage.setItem('Passwords', JSON.stringify(ob));
+        alert('succed')
+      } catch (error) {
+        alert(error)
+      }
+  }
+
+   async retrieveObject() {
+    try {
+      const value = await AsyncStorage.getItem('Passwords');
+      if (value !== null) {
+        // We have data!!
+        passwords = JSON.parse(value)
+        console.log(passwords);
+        return passwords
+      }
+    } catch (error) {
+      // Error retrieving data
+      return error
+    }
+  };
+
+  _storeData = async () => {
+    try {
+      await AsyncStorage.setItem('TASKS', 'I like to save it.');
+    } catch (error) {
+      // Error saving data
+    }
+  };
+
+  _retrieveData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('TASKS');
+      if (value !== null) {
+        // We have data!!
+        alert(value)
+        console.log(value);
+      }
+    } catch (error) {
+      // Error retrieving data
+    }
+  };
+
+
+
+
 }
 
 export default withNavigation(Homescreen);
